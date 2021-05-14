f5fa6317095ca4937a4d7fd100528816e173e4cd
Antonio Massa <antonio.massa@zehus.it>
2021-05-14 16:56:44 +0200

    Initial Commit

---
+++ Moonshoot/MoonshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshootApp.swift
+//  Moonshoot
+//
+//  Created by Massa Antonio on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
